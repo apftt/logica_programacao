bda959e6b97a1b4af154c772d1d8d610af8c075a
apftt <anapaula.ftto@gmail.com>
2023-08-20 23:13:54 -0300

    Atividade 04
    Gerenciamento de pessoas

---
+++ atividade_4.py
@@ -0,0 +1,10 @@
+#Enunciado: Você e sua equipe de programadores foram contratados por pequena empresa para desenvolver o software de gerencialme de pessoas. Este software deve ter o seguinte menu e opções:
+
+#    1) Cadastrar Colaborador
+#    2) Consultar Colaborador
+#        1. Consultar Todos 
+#        2. Consultar por Id;
+#        3. Consultar por Setor;
+#        4. Retornar ao menu;
+#    3) Remover Colaborador
+#    4) Encerrar Programa
