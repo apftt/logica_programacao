42257b5b6890e340bd25aeece12053f616738c05
apftt <anapaula.ftto@gmail.com>
2023-08-20 23:12:46 -0300

    Atividade 02
    Sorveteria

---
+++ atividade_2.py
@@ -0,0 +1,6 @@
+# Enunciado: Você e sua equipe de programadores foram contratados para desenvolver um app de vendas para uma sorveteria. Você ficou com a parte de desenvolver a interface do cliente para retirada do produto.
+# A Sorveteria possui seguinte relação:
+
+#  • 1 bola de sorvete no sabor tradicional (tr) custa 6 reais, no sabor premium (pr) 7 reais e no especial (es) 8 reais;
+#  • 2 bolas de sorvete no sabor tradicional (tr) custam 11 reais, no sabor premium (pr) 13 reais e no especial (es) 15 reais;
+#  • 3 bolas de sorvete no sabor tradicional (tr) custam 15 reais, no sabor premium (pr) 18 reais e no especial (es) 21 reais;
