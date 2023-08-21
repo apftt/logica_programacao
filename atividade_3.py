2b848da9edae6372a4c354408a1d3ee1acc74e4c
apftt <anapaula.ftto@gmail.com>
2023-08-20 23:13:17 -0300

    Atividade 03
    Petshop

---
+++ atividade_3.py
@@ -0,0 +1,20 @@
+#Enunciado: Você foi contratado para desenvolver um sistema de cobrança de banho para um petshop. Você ficou com a parte de desenvolver a interface com o funcionário.
+#O petshop opera da seguinte maneira:
+#    • Para cães com peso menor que 3 kg o valor base é de 40 reais;
+#    • Para cães com peso igual ou maior que 3 kg e menor que 10 kg o valor base é de 50 reais;
+#    • Para cães com peso igual ou maior que 10 kg e menor que 30kg o valor base é de 60 reais;  
+#    • Para cães com peso igual ou maior que 30 kg e menor que 50kg o valor base é de 70 reais; 
+
+#    • Para cães com pelo curto (c) o multiplicador é 1;
+#    • Para cães com pelo médio (m) o multiplicador é 1.5;
+#    • Para cães com pelo longo (l) o multiplicador é 2;
+
+#    • Para o adicional de cortar unhas (1) do cachorro é cobrado um valor extra de 10 reais;
+#    • Para o adicional de escovar os dentes (2) do cachorro é cobrado um valor extra de 12 reais;
+#    • Para o adicional de limpar as orelhas (3) do cachorro é cobrado um valor extra de 15 reais;
+#    • Para o adicional de não querer mais nada (0) é cobrado um valor extra de 0 reais;
+
+
+#O valor final da conta é calculado da seguinte maneira:
+
+#total = base * multiplicador + extra
