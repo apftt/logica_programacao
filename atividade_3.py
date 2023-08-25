9e4c77c248899844289d90f6f3375c812047d3df
apftt <anapaula.ftto@gmail.com>
2023-08-24 21:44:11 -0300

    Correção atividade 03
    Corrigido

--- atividade_3.py
+++ atividade_3.py
@@ -21,7 +21,7 @@
 
 # Mensagem boas-vindas
 def msg(x):
-    tam = len(x)
+    tam = len(x) # para ficar borda do tamanho do texto digitado
     if tam:
         print('+', '-' * tam, '+')
         print('|', x, '|')
@@ -30,20 +30,20 @@ def msg(x):
 # Definição do peso do cachorro
 def cachorro_peso():
     print('-------------------- PESO DO CACHORRO --------------------')
-    while True:
+    while True: #garante que a função retorne uma opção válida
         try:
-            base = float(input('Qual o peso do seu cachorro (Kg)? '))
-            if (base < 3):
+            peso = float(input('Qual o peso do seu cachorro (Kg)? ')) #caso seja digitado valor não inteiro
+            if (peso < 3):
                 return 40
-            elif (base >= 3) and (base < 10):
+            elif (peso >= 3) and (peso < 10):
                 return 50
-            elif (base >= 10) and (base < 30):
+            elif (peso >= 10) and (peso < 30):
                 return 60
-            elif (base >= 30) and (base < 50):
+            elif (peso >= 30) and (peso < 50):
                 return 70
             else:
                 print('Não aceitamos cachorros com peso superior a 50 kg.')
-        except ValueError:
+        except ValueError: #caso seja digitado letra ou valor inválido
             print('Favor digitar peso válido.')
 
 # Definição tamanho do pelo do animal
@@ -55,7 +55,8 @@ def cachorro_pelo():
                               'M - Pelo médio \n' +
                               'L - Pelo Longo \n' +
                               '>>  ')
-        multiplicador = multiplicador.upper()
+        multiplicador = multiplicador.upper() #transforma as opções digitadas em letras maiúsculas
+        # conforme a opção digitada abaixo retornará o multiplicador para cálculo
         if multiplicador == 'C':
             return 1
         elif multiplicador == 'M':
@@ -70,7 +71,7 @@ def cachorro_pelo():
 def cachorro_extra():
     print('\n-----------------------ADICIONAIS-------------------------')
     acumulador = 0
-    while True:
+    while True: #garante que a função retorne uma opção válida
         extra = input('Deseja algum serviço adicional? Favor digite o número do serviço desejado: \n' +
           '1 - Cortar unhas; \n' +
           '2 - Escovar os dentes; \n' +
@@ -87,7 +88,7 @@ def cachorro_extra():
             acumulador = acumulador + 15
             continue #volta para o início do loop
         elif extra == '0':
-            return acumulador
+            return acumulador #Para não zerar se tiver algum adicional já selecionado
         else:
             print('Opção inválida! Favor digitar uma opção existente.\n')
 
@@ -95,9 +96,9 @@ def cachorro_extra():
 # Início do Main
 msg('SEJA BEM-VINDO(A) AO PETSHOP DA ANA PAULA FERRONATTO')
 print('\nA seguir escolha as opções desejadas para o melhor banho no seu cachorro\n')
-base = cachorro_peso()
-multiplicador = cachorro_pelo()
-extra = cachorro_extra()
+base = cachorro_peso() #Pega o valor do peso
+multiplicador = cachorro_pelo() #pega o valor referente ao tamanho do pelo para multiplicação
+extra = cachorro_extra() #adicionada o valor da quantidade de adicionais escolhidos
 total = (base * multiplicador) + extra
-print('O valor total do banho é de R$ {:.2f}'.format(total))
+print('O valor total do banho é de R$ {:.2f} (peso: {} * pelo: {} + adicional(is): {})'.format(total,base,multiplicador,extra)) #coloca :.2f dentro das chaves para valor ficar com apenas duas casas decimais
 
